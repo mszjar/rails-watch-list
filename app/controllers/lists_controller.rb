78564d74eacc013cf63508532d3d92fc00ef96b2
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2022-11-17 23:23:12 +0100

    challenge done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,47 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[edit update show]
+
+  def index
+    @lists = params[:query] ? List.where("name LIKE '%#{params[:query]}%'") : List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @list = List.update(list_params)
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
